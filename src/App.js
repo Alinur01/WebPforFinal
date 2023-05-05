3ade27f1173eb74aea93c02e1aef0bfe1ad81391
Dnaeel <daneelshurubkov1@gmail.com>
2023-05-05 11:02:25 +0600

    completed the food-planet main block

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import MainBlock from "./components/Main-block";
 
 const App = () => {
     return (
         <>
             <div className="container">
                 <Header/>
+                <MainBlock/>
             </div>
         </>
     )
